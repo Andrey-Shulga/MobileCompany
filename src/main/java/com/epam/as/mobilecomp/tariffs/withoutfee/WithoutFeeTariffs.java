fee4f8eb6877917ddfa01cedea5381f27d3a0b81
Andrey Shulga <solyariz@gmail.com>
2016-10-06 14:50:47 +0600

    Added general class WithoutFeeTariffs

--- src/main/java/com/epam/as/mobilecomp/tariffs/withoutfee/WithoutFeeTariffs.java
+++ src/main/java/com/epam/as/mobilecomp/tariffs/withoutfee/WithoutFeeTariffs.java
@@ -3,11 +3,13 @@ package com.epam.as.mobilecomp.tariffs.withoutfee;
 import com.epam.as.mobilecomp.tariffs.Tariffs;
 
 /**
- * Class for tariffs without subscription fee.
+ * General class for tariffs without subscription fee.
  */
 public class WithoutFeeTariffs extends Tariffs {
 
-    private int callCost;
+    private int costCallInNet;
+    private int costCallOutNet;
+    private int costTraffic;
 
     /**
      * Construct new tariff without fee.
@@ -15,9 +17,14 @@ public class WithoutFeeTariffs extends Tariffs {
      * @param name      the name of new tariff
      * @param customers amount of customers
      * @param isHasFee  has or not fee
+     * @param costCallInNet cost of call in network per minute
+     * @param costCallOutNet cost of call out network pre minute
      */
-    WithoutFeeTariffs(String name, int customers, boolean isHasFee) {
+    public WithoutFeeTariffs(String name, int customers, boolean isHasFee, int costCallInNet, int costCallOutNet, int costTraffic) {
         super(name, customers, isHasFee);
+        this.costCallInNet = costCallInNet;
+        this.costCallOutNet = costCallOutNet;
+        this.costTraffic = costTraffic;
     }
 
     /**
