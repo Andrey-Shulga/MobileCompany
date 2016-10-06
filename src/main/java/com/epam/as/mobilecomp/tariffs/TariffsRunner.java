ddceb60a18ca697bab2d902d07e8e61eb4570542
Andrey Shulga <solyariz@gmail.com>
2016-10-06 15:31:53 +0600

    Added class TariffsRunner.

---
+++ src/main/java/com/epam/as/mobilecomp/tariffs/TariffsRunner.java
@@ -0,0 +1,14 @@
+package com.epam.as.mobilecomp.tariffs;
+
+/**
+ * This program demonstrates actions with tariffs of Mobile Company.
+ *
+ * @author Andrey Shulga
+ * @version 1.0 2016-10-06
+ */
+
+public class TariffsRunner {
+    public static void main(String[] args) {
+
+    }
+}
